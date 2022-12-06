18938613596703190228e212db2da64dadedd02a
Gabs Oliveira <gbrielrox@hotmail.com>
2022-12-06 16:33:29 -0300

    acrescimo api com lista de municipios e substituição do nome

--- codigo_fuzzy_matching.R
+++ codigo_fuzzy_matching.R
@@ -5,17 +5,52 @@ library('dplyr')
 library('readxl')
 library('stringr')
 library("stringi")
+library("httr")
+library("jsonlite")
+
+# FUNÇÕES
+
+## Função que recebe os dados da API e entrega um arquivo JSON
+extrai_api <- function(APIURL){
+  #Recebdno as informações da API
+  get_budget <- GET(APIURL, type = "basic")
+  
+  if(get_budget$status_code == 200){
+    #transformando as informações em texto
+    get_budget_text <- content(get_budget, "text")
+    
+    #Transformando o texto em JSON, pois assim transforma o texto (.txt) em um arquivo que pode ser lido em forma "tabela"
+    #O arquivo .JSON é criado com dois objetos, avisos de hoje e do futuro
+    get_budget_json <- fromJSON(get_budget_text, flatten = TRUE)
+    
+    get_budget_df <- as.data.frame(get_budget_json)
+    
+    return(get_budget_df)
+  } else {
+    print("Erro na conexão da api")
+  }
+  
+}
+
+#************************************************#
+
+# URL da api
+urlapimuni <- "https://servicodados.ibge.gov.br/api/v1/localidades/estados/MG/distritos"
+#busca a lista de municipios para MG, para outros tipos de acesso conferir em https://servicodados.ibge.gov.br/api/docs/localidades
 
 
-municipios_mg <- read_excel("C:/Users/Gbrie/Workspace/fuzzy_matching/base/municipios_mg.xlsx", 
-                            col_types = c("numeric", "text", "text", 
-                                          "text", "text"))
+#Recebendo o DF com os municipios de MG
+municipios_mg <- extrai_api(urlapimuni)
 
-
-
+municipios_mg <- municipios_mg %>% 
+                  select(municipio.id, municipio.nome, municipio.microrregiao.id, municipio.microrregiao.nome,
+                         municipio.microrregiao.mesorregiao.id, municipio.microrregiao.mesorregiao.nome,
+                         ) %>% 
+                    distinct(municipio.id, .keep_all = TRUE)
+                      
 #criando uma coluna com os nomes maiusculos e sem espaço
 municipios_mg <- municipios_mg %>%
-                    mutate(MUNICIPIO = str_replace_all(str_to_upper(stri_trans_general(municipios_mg$municipio, 'latin-ascii')), " ", ""))
+                    mutate(MUNICIPIO = str_replace_all(str_to_upper(stri_trans_general(municipios_mg$municipio.nome, 'latin-ascii')), " ", ""))
                     
 
 exemplo <- data.frame(muni = c("Presidnte Bernardes", "Tre Corações", "Piedade do Reio Grande", "Silverania"))
@@ -31,4 +66,3 @@ for(x in 1:length(linha_match)){
   linha <- municipios_mg[linha_match[x], 2]
   exemplo[x, 1] <- linha
 }
-
